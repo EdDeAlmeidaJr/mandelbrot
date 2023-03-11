ac5af43e733cbe845d610d7dd544fa08a0785c51
Ed de Almeida <edvaldoajunior@gmail.com>
2023-03-11 00:15:32 -0300

    Initial commit

---
+++ backup/unit1.pas
@@ -0,0 +1,53 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    procedure FormCreate(Sender: TObject);
+    procedure FormPaint(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.FormPaint(Sender: TObject);
+var
+  xmin, xmax, ymin, ymax: Float;
+  i: Integer;
+  j: Integer;
+begin
+  for i:=0 to 500 do
+  begin
+    for j:=0 to 500 do
+    begin
+      Canvas.Pixels[i,j] := $FFFF00 + i + j;
+    end
+  end;
+end;
+
+end.
+
