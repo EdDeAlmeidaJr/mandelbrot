35ba0c0c2be7aa3d56c4a3a30b56f78a0a920f68
Ed de Almeida <edvaldoajunior@gmail.com>
2023-03-11 10:35:36 -0300

    Calculations

--- unit1.pas
+++ unit1.pas
@@ -5,14 +5,13 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
-    procedure FormCreate(Sender: TObject);
     procedure FormPaint(Sender: TObject);
   private
 
@@ -29,23 +28,78 @@ implementation
 
 { TForm1 }
 
-procedure TForm1.FormCreate(Sender: TObject);
+function StartIncrement: Single;
 begin
+  result := 0.0001;
+end;
 
+function Tx(x, maxx: Single): Integer;
+begin
+  result := Round(((x + maxx) * Form1.Width) / ( 2 * maxx));
+end;
+
+function Ty(y, maxy: Single): Integer;
+begin
+  result := Round(((y - maxy) * Form1.Height) / (2 * maxy));
+end;
+
+function SensibilityX(maxx: Single) : Single;
+var
+  i1, i2 : Integer;
+  x, inc : Single;
+begin
+  x := 0;
+  i1:= Tx(x, maxx);
+  inc:= StartIncrement;
+  while (true) do
+  begin
+    i2 := Tx(x + inc, maxx);
+    if (i2 = (i1 + 1)) then Break;
+    inc := 2 * inc;
+  end;
+  result := inc;
+end;
+
+function SensibilityY(maxy: Single) : Single;
+var
+  j1, j2 : Integer;
+  y, inc : Single;
+begin
+  y := 0;
+  j1:= Ty(y, maxy);
+  inc:= StartIncrement;
+  while (true) do
+  begin
+    j2 := Ty(y + inc, maxy);
+    if (j2 = (j1 + 1)) then Break;
+    inc := 2 * inc;
+  end;
+  result := inc;
 end;
 
 procedure TForm1.FormPaint(Sender: TObject);
 var
-  xmin, xmax, ymin, ymax: Single;
-  i: Integer;
-  j: Integer;
+  x, y, maxx, maxy, sensx, sensy: Single;
+  i, j: Integer;
 begin
-  for i:=0 to 500 do
+  maxx := 3.0;
+  maxy := 2.0;
+  sensx := SensibilityX(maxx);
+  sensy := SensibilityY(maxy);
+  x:= 0;
+  while (true) do
   begin
-    for j:=0 to 500 do
+    i := Tx(x, maxx);
+    y := 0;
+    while (true) do
     begin
-      Canvas.Pixels[i,j] := $FFFF00 + i + j;
-    end
+      j := Ty(y, maxy);
+      Canvas.Pixels[i,j] := $0000FF;
+      y := y + sensy;
+      if (y > maxy) then Break;
+    end;
+    x := x + sensx;
+    if (x > maxx) then Break;
   end;
 end;
 
